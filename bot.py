3222d5446bde4a4cd8c46dfe2e18c3f238c7f43b
alake16 <aclake@umich.edu>
2019-12-01 23:23:11 -0600

    Tweeting CPU temperature

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import sys
+import os
 from twython import Twython
 
 #Define our constant variables, this is all the data we wrote down in the first part of the tutorial.
@@ -10,6 +11,13 @@ ACCESS_SECRET = 'J2fzNd3vIyTWEw3p7q3vKZ9H1iN7V0vKP6nC9R6jZmROQ'
 
 #Create a copy of the Twython object with all our keys and secrets to allow easy commands.
 api = Twython(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_KEY,ACCESS_SECRET) 
+
+cmd = '/opt/vc/bin/vcgencmd measure_temp'
+line = os.popen(cmd).readline().strip()
+tempC = line.split('=')[1].split("'")[0]
+tempF = (float(temp) * (9 / 5)) + 32
+
+tempTweet = "Current CPU temperature: {} deg F".format(tempF)
 
 #Using our newly created object, utilize the update_status to send in the text passed in through CMD
 api.update_status(status=sys.argv[1])
